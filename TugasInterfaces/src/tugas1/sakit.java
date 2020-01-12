8eec06e4821a92c6efd58084342f30ee059e59aa
Avanul <56856612+Avanul@users.noreply.github.com>
2020-01-12 15:26:52 +0700

    Add files via upload

---
+++ TugasInterfaces/src/tugas1/sakit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author windows
+ */
+public class sakit implements demam{
+      public void Batuk() {
+    // The body of animalSound() is provided here
+    System.out.println("ugh");
+  }
+  public void Flu() {
+    // The body of sleep() is provided here
+    System.out.println("Hachu");
+  }
+}
